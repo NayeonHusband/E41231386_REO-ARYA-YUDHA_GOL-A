c525393c514260eb30de55bc6a5eecd453736254
NayeonHusband <110769248+NayeonHusband@users.noreply.github.com>
2024-02-12 10:13:46 +0700

    masuk

---
+++ Minggu-1/JavaApplication31/src/javaapplication31/JavaApplication31.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication31;
+
+public class JavaApplication31 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
